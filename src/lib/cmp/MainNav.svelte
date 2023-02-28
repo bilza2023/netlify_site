9128d8b15272e7bc3940188e669f872e4e963e4c
unknown <bilza32611@gmail.com>
2023-02-28 14:49:01 +0500

    fixed mobile menu

--- src/lib/cmp/MainNav.svelte
+++ src/lib/cmp/MainNav.svelte
@@ -1,18 +1,27 @@
+<script>
+
+let isVisible = false;
+
+</script>
+
 <nav class="bg-gray-700 shadow-lg">
 
   <div class="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
+
     <div class="flex justify-between items-center">
       <div>
         <a class="text-xl font-bold text-white" href="/">skillzaa.com</a>
       </div>
 
       <div class="md:hidden">
-        <button type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
+
+        <button  type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none" on:click={()=> isVisible = !isVisible }>
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
             <path v-if="!isOpen" fill-rule="evenodd" clip-rule="evenodd" d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"></path>
             <path v-else fill-rule="evenodd" clip-rule="evenodd" d="M6 18L18 6M6 6l12 12"></path>
           </svg>
         </button>
+      
       </div>
     </div>
 
@@ -28,7 +37,9 @@
     </div>
   </div>
 
-  <div class="hidden md:hidden">
+
+{#if isVisible}
+  <div id="mobile-menu" class="">
     <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
       <a class="text-white hover:text-gray-300 py-2 block" href="/">Home</a>
       <a class="text-white hover:text-gray-300 py-2 block" href="/blog">Blog</a>
@@ -36,5 +47,6 @@
       <a class="text-white hover:text-gray-300 py-2 block" href="/labs">Labs</a>
     </div>
   </div>
+{/if}
 
 </nav>
