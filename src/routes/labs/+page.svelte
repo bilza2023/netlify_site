e38f8408ba0d28357c23b9b08165f3a38f9308ee
unknown <bilza32611@gmail.com>
2023-03-03 01:58:22 +0500

    api call

--- src/routes/labs/+page.svelte
+++
@@ -1,4 +0,0 @@
-
-
-
-<h1>This Section will have all the experimental stuff</h1>
---
+++ src/routes/labs/api_call/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+  console.log(data);
+</script>
+
+
+{#each data.cities as city }
+    
+<h1>{city.name}</h1>
+<hr/>
+{/each} 
