8876946eecdd466e5f77865d4094a6b4377fe955
unknown <bilza32611@gmail.com>
2023-03-03 02:54:01 +0500

    post test

--- src/routes/labs/api_call/+page.svelte
+++ src/routes/labs/api_call/+page.svelte
@@ -1,6 +1,25 @@
 <script>
   export let data;
+  let cityName = "city";
   console.log(data);
+
+const deposit = async ()=>{
+
+  try {
+    const response = await fetch('https://skillzaa.cyclic.app/add_city', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name :cityName})
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 
@@ -9,3 +28,9 @@
 <h1>{city.name}</h1>
 <hr/>
 {/each} 
+
+
+
+<input type="text" bind:value={cityName}>
+
+<button on:click={deposit}>Add City</button>
