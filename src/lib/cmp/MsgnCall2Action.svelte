78ac7049e6342a68fb4b44aa087c2b838cb058df
unknown <bilza32611@gmail.com>
2023-03-08 23:24:50 +0500

    may have fixed intro - screen

--- src/lib/cmp/MsgnCall2Action.svelte
+++ src/lib/cmp/MsgnCall2Action.svelte
@@ -1,5 +1,4 @@
 <script>
-  
   export let background_color = "bg-gray-600";
   export let text_color = "text-white";
   export let button_content = "Call To Action";
@@ -9,10 +8,21 @@
   // const isSmallScreen = media("(max-width: 640px)");
 </script>
 
-<div class="flex flex-col justify-center items-center mt-8 ">
-  <div class="text-xl p-8  font-bold text-center mb-4 {text_color} w-1/2 md:w-3/4 rounded-lg p-2 {background_color} ">
+<style>
+  .flex-col {
+    flex-grow: 1;
+  }
+
+  .text-xl {
+    max-width: 100%;
+    width: auto;
+  }
+</style>
+
+<div class="flex flex-col justify-center items-center mt-1 flex-col">
+  <div class="text-xl p-8 font-bold text-center mb-4 {text_color} w-1/2 md:w-3/4 rounded-lg p-2 {background_color} ">
     <slot></slot>
-    <hr class=" pb-2" />
+    <hr class="pb-2" />
     <button class="mt-4 mb-2 w-full py-2 rounded-md {button_color} text-white font-bold hover:bg-red-600 transition-colors duration-300"
     on:click={actionHandler}>
     {button_content}</button>
