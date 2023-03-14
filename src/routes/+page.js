be3822391cff201236aaa612afa9686446ee8197
unknown <bilza32611@gmail.com>
2023-03-14 12:27:20 +0500

    its built

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,14 +3,14 @@ import {is_login} from "$lib/stores/appStore.js";
 
 export async function load({ params }) {
 
-try {
-    const token = localStorage.getItem("token");
-        if (token == null || token.length == 0) {
-            is_login.set(false);
-        }else {
-            is_login.set(true);
-        }
-  } catch (error) {
-    console.error(error);
-  }
+// try {
+//     const token = localStorage.getItem("token");
+//         if (token == null || token.length == 0) {
+//             is_login.set(false);
+//         }else {
+//             is_login.set(true);
+//         }
+//   } catch (error) {
+//     console.error(error);
+//   }
 }
