d5a3d387874b672f8e070c419cdc58c95de9a0e2
unknown <bilza32611@gmail.com>
2023-03-15 13:04:15 +0500

    in the name of Allah

--- src/lib/MainFlex.svelte
+++ src/lib/MainFlex.svelte
@@ -5,7 +5,7 @@
 <div class="flex">
 
 <div class="card">
-<a href="http://localhost:5173/quiz/show?quizId=6411609828a369b541fcd7d7">Test</a>
+<a href="/quiz/show?quizId=6411609828a369b541fcd7d7">Test</a>
 
 </div>
 
