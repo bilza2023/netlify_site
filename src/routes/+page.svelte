6f7ff3840eb10adb4c5984523b34d134f1d4b5f5
unknown <bilza32611@gmail.com>
2023-03-18 03:49:10 +0500

    moved an inch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 import MainFlex from "$lib/MainFlex.svelte";
+import Underconstruction from "$lib/cmp/Underconstruction.svelte";
+
 import {mainQuizList} from "./main_quiz_list.js";
-console.log("QuizList" , mainQuizList);
+// console.log("QuizList" , mainQuizList);
 
 // import {is_login} from "$lib/stores/appStore.js";
 // let isLogin =false;
@@ -14,7 +16,7 @@ console.log("QuizList" , mainQuizList);
 <!-- <h1><a href="/quiz/create">New Quiz</a></h1> -->
 <!-- <h1><a href="/quiz">Quizzes</h1> -->
 <!-- <button on:click={add}>Add</button> -->
-
+<Underconstruction  />
 <img src="./images/heroBanner.png" alt="">
 
 <br/>
