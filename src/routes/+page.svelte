e3864b485e8745aa0dfe895d9b30d305ac76dfb8
unknown <bilza32611@gmail.com>
2023-03-30 16:29:37 +0500

    in the life

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,15 @@ import  generateRandomGradient  from "$lib/js/gradiant_dark.js";
 // import DivCard from "$lib/cmp/DivCard.svelte";
 // import {mainQuizList} from "./main_quiz_list.js";
 import { BASE_URL } from '$lib/js/config.js';
-import { onMount } from 'svelte';
-// console.log("QuizList" , mainQuizList);
-let quizzes;
+
+const quizzes = [
+{ _id : "64234b9db5d31dab8acf14bb" , title : "World Geography Facts"},
+{ _id : "6424bc4cc51d2f76484c3ff7" , title : "World War II Facts"},
+{ _id : "6424018be683525d966ea647" , title : "Our Solar System"},
+{ _id : "6424018be683525d966ea649" , title : "Global Economy 101"}
+
+];
+//...
 const skrink = (txt)=>{
   if (txt.length <= 25) {
     return txt;
@@ -16,13 +22,7 @@ const skrink = (txt)=>{
     return txt.slice(0, 22) + "...";
   }    
 }
-
-onMount(async () => {
- const resp = await fetch( `${BASE_URL}/quiz/featured`);
-const q  = await resp.json();
-quizzes = q.quizzes;
-// debugger;
-}); 
+ 
 </script>
 
 
@@ -33,28 +33,24 @@ quizzes = q.quizzes;
 <br>
 <br>
 
-
-{#if quizzes}
-
 <div class="flex flex-wrap  justify-center gap-2">
 
 {#each quizzes as item}
-  <a href={`/quiz/show?quizId=${item._id}`} 
+<a href={`/quiz/show?quizId=${item._id}`} 
   class="flex-shrink-0   w-full md:w-1/2 lg:w-1/5  ">
-    <div class=" rounded-lg cursor-pointer border-2 border-white text-center">
-      
-      <div style={`min-height:100px; background-image: ${generateRandomGradient()}`} class="flex items-center rounded-md justify-center">
-  <p class=" overflow-hidden overflow-ellipsis">{skrink(item.title)}</p>
-</div>
 
-    </div>
-  </a>
+<div class=" rounded-lg cursor-pointer border-2 border-white text-center">
+      
+<div style={`min-height:100px; background-image: ${generateRandomGradient()}`} class="flex items-center rounded-md justify-center">
+<p class=" overflow-hidden overflow-ellipsis">{skrink(item.title)}</p>
+
+</div>
+</div>
+</a>
 
 {/each}
 
 </div>
 
-{:else}
-  <p class="p-4 m-4 w-full bg-gray-500 border-2 border-gray-200 text-2xl text-center rounded-md">Loading...</p>
-{/if}
+  <!-- <p class="p-4 m-4 w-full bg-gray-500 border-2 border-gray-200 text-2xl text-center rounded-md">Loading...</p> -->
 
