a9fc4d1c9c532b778d249cd5e724f93086b81729
unknown <bilza32611@gmail.com>
2023-04-02 04:37:46 +0500

    in the name of Allah

--- src/lib/cmp/ThreeCards.svelte
+++ src/lib/cmp/ThreeCards.svelte
@@ -1,8 +1,7 @@
 
+<div class="flex flex-wrap gap-4 justify-center md:justify-between">
 
-<div class="flex gap-2 justify-between">
-
-<div class="flex-1 border-2 border-white rounded-md p-2 m-2 text-center">
+<div class="w-full md:w-1/3 border-2 border-white rounded-md p-4 text-center">
 
     <svg class="mx-auto"  xmlns="http://www.w3.org/2000/svg" width="150" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="1.25" d="M20 7c1.25 1.67 2 3.75 2 6c0 5.52-4.48 10-10 10S2 18.52 2 13S6.48 3 12 3m0-2v12l9.6-7.2C19.41 2.89 15.92 1 12 1Z"/></svg>
 
@@ -20,7 +19,7 @@
 </div>
 
 
-<div class="flex-1 border-2 border-white rounded-md p-2 m-2 text-center">
+<div class="w-full md:w-1/3 border-2 border-white rounded-md p-4 text-center mt-4 md:mt-0">
 
 <svg class="mx-auto" width="150"  preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-width="1.25" d="M18 8a3 3 0 1 0 0-6a3 3 0 0 0 0 6ZM6 15a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm12 7a3 3 0 1 0 0-6a3 3 0 0 0 0 6Zm-2-4l-8-5m8-7l-8 5"/></svg>
 
