bcbca0604b5548b6ede94224de332e6bcd718c19
unknown <bilza32611@gmail.com>
2023-04-19 10:20:38 +0500

    ok

--- src/lib/nav/Nav.svelte
+++ src/lib/nav/Nav.svelte
@@ -49,7 +49,7 @@ isLogin = false ;
 <div class="w-15">
     <button class="w-full flex flex-col items-center p-2 m-0  rounded hover:bg-gray-500 active:bg-gray-900"
     on:click={logoutFn}>
-      <span class="text-md">&#x27A1;</span>
+      <span class="text-md">&#x1F6EB;</span>
       <span class="text-xs font-md text-white">Logout</span>
     </button>
 </div>
