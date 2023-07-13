2554a27ed9211fd58721a7ee742676d54361e065
unknown <bilza32611@gmail.com>
2023-07-13 17:16:07 +0500

    in the line of fire

--- src/routes/show/+page.svelte
+++ src/routes/show/+page.svelte
@@ -1,34 +1,47 @@
 <script>
 
-// import FormIntro from "./formIntro/FormIntro.svelte";
-// import QuizComp from "./quizComp/QuizComp.svelte";
-// import Outro from "./outro/Outro.svelte";
- 
-// import { BASE_URL } from '$lib/js/config.js';
 import Loading from '$lib/cmp/Loading.svelte';
-import Agent from '../../lib/communicator/Agent';
 import { onMount } from 'svelte';
+import { BASE_URL } from '../../lib/js/config.js';
+import {pageStateStore} from "./store";
+///////////////////////////////////////////////////
+import LoginForm from './loginForm/LoginForm.svelte';
+import Goodbye from './goodbye/Goodbye.svelte';
+import ShowQuiz from './showQuiz/ShowQuiz.svelte';
+import Result from './result/Result.svelte';
 
-let survey;
-let pageState = 'loading';
-let notFoundMsg = "Not Found";
+$: pageState = $pageStateStore;
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+let quiz;
+let students;
 
 onMount(async () => {
   try {
-  debugger;
-  let  quizId = new URLSearchParams(location.search).get("quizId"); 
-  const resp = await Agent.readone('run', {id :quizId});
-
+    // debugger;
+    pageStateStore.set('loading');
+    let  quizId = new URLSearchParams(location.search).get("quizId"); 
+      const resp = await fetch( `${BASE_URL}/show/${quizId}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer 000`,
+        }
+      });
+  
     if (resp.ok) {
+        pageStateStore.set('loaded');
         const data = await resp.json();
-        survey = data.item; 
+        quiz = data.quiz;
+        students = data.students;
+        // console.log(data);
     } else {
       const data = await resp.json();
-      toast.push('failed to load');
+      toast.push(data.message);
     }
   
   } catch (error) {
-    console.error(error);
+    // console.error(error);
   }
 }); 
 
@@ -37,9 +50,31 @@ onMount(async () => {
 
 <div class="wrapper w-full p-2 bg-gray-800 min-h-screen text-white ">
 
-<h1>This is 100%</h1>
-{#if survey}
-<h1>{survey.title}</h1>
+{#if quiz}
+  
+          <!-- //==loaded======-->    
+            {#if pageState == 'loaded'}
+              <LoginForm {quiz}{students} />
+            {/if}
+          
+          <!-- //==showQuiz======-->    
+            {#if pageState == 'showQuiz'}
+              <!-- <ShowQuiz {quiz} /> -->
+              <h1>ShowQuiz</h1>
+            {/if}
+
+            <!-- //==result======-->  
+            {#if pageState == 'result'}
+              <Result {quiz} />
+            {/if}
+
+            <!-- //=goodbye======-->
+            {#if pageState == 'goodbye'}
+              <Goodbye  {quiz}/>
+            {/if}
+
+{:else}
+<Loading />
 {/if}
 </div>
 
---
+++ src/routes/show/store.js
@@ -0,0 +1,8 @@
+
+import { writable } from 'svelte/store';
+
+export const pageStateStore = writable('loading');
+export const emailStore = writable('');
+// export const passwordStore = writable('');
+
+
